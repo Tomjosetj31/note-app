35b2ac81d64a7ae7663224c5946733f21cecf1d7
Tomjosetj31 <tomjosethomastj31@gmail.com>
2023-10-05 23:53:53 +0530

    rename app

--- note_app/urls.py
+++ note_app/urls.py
@@ -1,4 +1,4 @@
-"""quizz_app URL Configuration
+"""note_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- note_app/wsgi.py
+++ note_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quizz_app project.
+WSGI config for note_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizz_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "note_app.settings")
 
 application = get_wsgi_application()
